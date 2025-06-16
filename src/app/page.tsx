24b4eb64f346c028317bbd4becf4b38e8707d7e5
Joshua Goon <jgoon2004@gmail.com>
2025-06-16 02:05:27 -0700

    add disclaimer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,11 +43,13 @@ const data = [
 
 export default function Home() {
   const [selections, setSelections] = useState(Array(data.length).fill(null));
-  const [email, setEmail] = useState("");
   const [errors, setErrors] = useState<string[]>([]);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isShow, setIsShow] = useState(false);
   const [isVerified, setIsVerified] = useState(false);
+  const [isAlreadySubmitted, setIsAlreadySubmitted] = useState(
+    Array(data.length).fill(null)
+  );
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -59,14 +61,10 @@ export default function Home() {
       setErrors((prev) => [...prev, "all selections must be made"]);
     }
 
-    // check if email is valid
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(email)) {
-      setErrors((prev) => [...prev, "email must be valid"]);
-    }
-
     // submit predictions
     setIsSubmitting(false);
+
+    // setIsShow(true);
   };
 
   return (
@@ -85,7 +83,7 @@ export default function Home() {
           >
             cluely
           </a>{" "}
-          on roy lee
+          on us <s>roy lee</s> (pending approval)
         </p>
 
         <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4">
@@ -169,89 +167,53 @@ export default function Home() {
               )}
 
               {isVerified && (
-              <button
-                type="submit"
+                <button
+                  type="submit"
                   disabled={isSubmitting}
-                className="w-full py-2 px-4 bg-[#fb651e] text-white rounded-md hover:bg-[#fb651e]/90 disabled:opacity-50"
-              >
-                {isSubmitting ? (
-                  <div className="flex items-center justify-center">
-                    <svg
-                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
-                    submitting predictions...
-                  </div>
-                ) : (
-                  "submit predictions"
-                )}
-              </button>
+                  className="w-full py-2 px-4 bg-[#fb651e] text-white rounded-md hover:bg-[#fb651e]/90 disabled:opacity-50"
+                >
+                  {isSubmitting ? (
+                    <div className="flex items-center justify-center">
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                      submitting predictions...
+                    </div>
+                  ) : (
+                    "submit predictions"
+                  )}
+                </button>
               )}
             </>
           )}
         </form>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="row-start-4 flex gap-[24px] flex-wrap justify-center w-full max-w-[600px] font-mono">
+        <h1 className="font-bold">sweepstakes disclaimer</h1>
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          select yes or no to place your prediction for each event. one
+          individual who gets the most predictions correct (in DAY01 or DAY02)
+          will receive $100 value in ai tool subscriptions or one year of Cluely
+          (pending approval). winners will be announced at the end of the event.
+          proof of yc sus acceptance will be required to claim the prize. good
+          luck!
+        </p>
       </footer>
     </div>
   );
