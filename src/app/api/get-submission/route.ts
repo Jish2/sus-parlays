2aeb37bd1cea17f4a3240187f7dbd13bb6e534dd
Joshua Goon <jgoon2004@gmail.com>
2025-06-16 03:56:18 -0700

    fix lint errors

--- src/app/api/get-submission/route.ts
+++ src/app/api/get-submission/route.ts
@@ -2,14 +2,17 @@ import { AuthenticatedRequest, withAuth } from "@/middleware/auth";
 import supabase from "@/lib/db";
 
 export const GET = withAuth(async (req: AuthenticatedRequest) => {
-    try {
-        const phone = req.user.phone
+  try {
+    const phone = req.user.phone;
 
-        const { data, error } = await supabase.from('submissions').select('*').eq('phone', phone).limit(1);
+    const { data } = await supabase
+      .from("submissions")
+      .select("*")
+      .eq("phone", phone)
+      .limit(1);
 
-        return Response.json({ data });
-    }
-    catch (err) {
-        return Response.json({ error: (err as Error).message }, { status: 500 });
-    }
+    return Response.json({ data });
+  } catch (err) {
+    return Response.json({ error: (err as Error).message }, { status: 500 });
+  }
 });
--- src/app/api/get-vote-counts/route.ts
+++ src/app/api/get-vote-counts/route.ts
@@ -1,17 +1,16 @@
-import { AuthenticatedRequest, withAuth } from "@/middleware/auth";
+import { withAuth } from "@/middleware/auth";
 import supabase from "@/lib/db";
 
-export const GET = withAuth(async (req: AuthenticatedRequest) => {
-    try {
-        const { data, error } = await supabase.rpc('get_parlay_vote_counts');
+export const GET = withAuth(async () => {
+  try {
+    const { data } = await supabase.rpc("get_parlay_vote_counts");
 
-        const result = [];
-        for (let i = 0; i < data.length; i++) {
-            result.push({ "yes": data[i].yes, "no": data[i].no });
-        }
-        return Response.json({ result });
+    const result = [];
+    for (let i = 0; i < data.length; i++) {
+      result.push({ yes: data[i].yes, no: data[i].no });
     }
-    catch (err) {
-        return Response.json({ error: (err as Error).message }, { status: 500 });
-    }
+    return Response.json({ result });
+  } catch (err) {
+    return Response.json({ error: (err as Error).message }, { status: 500 });
+  }
 });
